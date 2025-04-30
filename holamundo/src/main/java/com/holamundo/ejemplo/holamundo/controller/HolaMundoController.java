b18f44b89f73f841fbcc0a52c2a1c980435841cf
munozmoranicolasprofesor <128549596+munozmoranicolasprofesor@users.noreply.github.com>
2025-04-30 19:43:54 -0400

    fix moreno

--- holamundo/src/main/java/com/holamundo/ejemplo/holamundo/controller/HolaMundoController.java
+++ holamundo/src/main/java/com/holamundo/ejemplo/holamundo/controller/HolaMundoController.java
@@ -8,7 +8,7 @@ public class HolaMundoController {
 
     @GetMapping("/hola")
     public String homaMundo() {
-        //kjfksjakljasdlas
+        //moreno
         return "¡Hola Mundo!";
     }
 }
